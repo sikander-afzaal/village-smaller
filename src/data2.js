4c095cfa3986d49ccfbd97556ac8d99619f818c7
Sikander Afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2023-08-02 13:22:14 +0500

    asd#	modified:   src/data.js

--- src/data2.js
+++ src/data2.js
@@ -2,15 +2,15 @@ export const data2 = {
   andhrapradesh: [
     {
       name: "Sivapuram",
-      pdf: "/Assets/andhra/Ventrapragada/pdf.pdf",
-      profileImg: "/Assets/andhra/Ventrapragada/profile.webp",
+      pdf: "/Assets/andhra/ventrapragada-village/pdf.pdf",
+      profileImg: "/Assets/andhra/ventrapragada-village/profile.webp",
       profileName: "Dr. Movva",
       images: [
-        "/Assets/andhra/Ventrapragada/img (1).webp",
-        "/Assets/andhra/Ventrapragada/img (2).webp",
-        "/Assets/andhra/Ventrapragada/img (3).webp",
-        "/Assets/andhra/Ventrapragada/img (4).webp",
-        "/Assets/andhra/Ventrapragada/img (5).webp",
+        "/Assets/andhra/ventrapragada-village/img (1).webp",
+        "/Assets/andhra/ventrapragada-village/img (2).webp",
+        "/Assets/andhra/ventrapragada-village/img (3).webp",
+        "/Assets/andhra/ventrapragada-village/img (4).webp",
+        "/Assets/andhra/ventrapragada-village/img (5).webp",
       ],
     },
   ],
